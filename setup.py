4c76a6d3fbaa4fcd721ff21409507330c2207098
Alpha <alpha@pokesplash.net>
2014-12-26 20:59:27 -0500

    Added setup and requirements for building package later

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pressure-sprites',
+      version='0.1',
+      description='Spritesheet Generator using Pressure',
+      url='http://github.com/Alphadelta14/python-pressure-sprites',
+      author='Alpha',
+      author_email='alpha@projectpokemon.org',
+      license='MIT',
+      packages=['pressure_sprites'],
+      zip_safe=True)
